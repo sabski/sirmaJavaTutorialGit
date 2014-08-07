44f320772ba585db168e0969768084cda50bb291
Simeon Iliev <sisabski23@gmail.com>
2014-08-07 10:18:55 +0300

    ITTE-945 Created gready regex matcher for html like text.

---
+++ JavaCourse/Reflection_Annotations_RegEx/src/main/java/com/sirma/itt/javacourse/reflectionAnotationRegex/task7/greadyRegex/RunGreedyRegexMatcher.java
@@ -0,0 +1,19 @@
+package com.sirma.itt.javacourse.reflectionAnotationRegex.task7.greadyRegex;
+
+import com.sirma.itt.javacourse.Utils;
+
+/**
+ * @author simeon
+ */
+public class RunGreedyRegexMatcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Utils.printConsoleMessage(Utils.editStringWithRegex(Utils.readLine(),
+				Utils.REGEX_VALIDATOR_X_TAGS, "<x/>"));
+	}
+
+}

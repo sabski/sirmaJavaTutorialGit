1648c52b3ee0d8844a2c66ab14232ed867e12919
Simeon Iliev <sisabski23@gmail.com>
2014-08-08 17:02:50 +0300

    ITTE-956 -  Adding a file reverser class and a runner class.

---
+++ JavaCourse/InputOutput/src/main/java/com/sirma/itt/javacourse/inputoutput/task3/reverseTextFile/FileReverser.java
@@ -0,0 +1,72 @@
+package com.sirma.itt.javacourse.inputoutput.task3.reverseTextFile;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sirma.itt.javacourse.Utils;
+
+/**
+ * Reverses the contents of a specific file.
+ * 
+ * @author simeon
+ */
+public class FileReverser {
+
+	/**
+	 * Reverses the contents of a specific text file.
+	 * 
+	 * @param fileName
+	 *            the name of the file we want to reverse its content.
+	 */
+	public void reverseFile(String fileName) {
+
+		File file = new File(fileName);
+		if (!file.exists() || file.isDirectory()) {
+			Utils.printConsoleMessage("Invalid file name");
+			return;
+		}
+		BufferedReader reader = null;
+		BufferedWriter writer = null;
+		try {
+
+			reader = new BufferedReader(new FileReader(file));
+			String line = null;
+			StringBuffer stringBuffer = new StringBuffer();
+			while ((line = reader.readLine()) != null) {
+				Utils.printConsoleMessage(line);
+				stringBuffer.append(line + "\n");
+
+			}
+			writer = new BufferedWriter(new FileWriter(file));
+			writer.write(stringBuffer.reverse().toString());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+}
---
+++ JavaCourse/InputOutput/src/main/java/com/sirma/itt/javacourse/inputoutput/task3/reverseTextFile/RunFileReverser.java
@@ -0,0 +1,23 @@
+package com.sirma.itt.javacourse.inputoutput.task3.reverseTextFile;
+
+import com.sirma.itt.javacourse.Utils;
+
+/**
+ * Class that runs the file reverser.
+ * 
+ * @author simeon
+ */
+public class RunFileReverser {
+
+	/**
+	 * @param args
+	 *            argument for the main method.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileReverser reverser = new FileReverser();
+		Utils.printConsoleMessage("Input the name of the file you want to reverse.");
+		reverser.reverseFile(Utils.readLine());
+	}
+
+}

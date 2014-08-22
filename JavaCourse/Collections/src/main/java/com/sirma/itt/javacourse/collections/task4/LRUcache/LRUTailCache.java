fb8f0732d99dbc3b646f5be89fa6c5d31c3f2171
Simeon Iliev <sisabski23@gmail.com>
2014-08-22 15:50:22 +0300

    ITTE-988 - Updated LRUTail.class now uses Queue - Changed List to Queue - Added coments to LRUTailCAche

--- JavaCourse/Collections/src/main/java/com/sirma/itt/javacourse/collections/task4/LRUcache/LRUTailCache.java
+++ JavaCourse/Collections/src/main/java/com/sirma/itt/javacourse/collections/task4/LRUcache/LRUTailCache.java
@@ -1,37 +1,52 @@
 package com.sirma.itt.javacourse.collections.task4.LRUcache;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 import com.sirma.itt.javacourse.Utils;
 
 /**
+ * LRU implementation class.
+ * 
  * @param <K>
+ *            the key value for the objects.
  * @param <V>
+ *            the Object type value that is to be stored in the cache.
  * @author simeon
  */
 public class LRUTailCache<K, V> {
 
 	private Map<K, V> cache;
-	private List<K> tail;
+	private Queue<K> queue;
 	private int maxElementts;
 
 	/**
+	 * Constructor for LRUTailCache class.
+	 * 
 	 * @param maxElementts
+	 *            the maximum number of elements allowed in the cache
 	 */
 	public LRUTailCache(int maxElementts) {
 		this.maxElementts = maxElementts;
 		cache = new HashMap<K, V>(maxElementts);
-		tail = new ArrayList<K>();
+		queue = new LinkedList<K>();
 	}
 
 	/**
+	 * Adds an element to the cache. First we check if the element is in the cache if it it is we
+	 * hit it, otherwise we check the size of the cache if its full or not, if the cache is not full
+	 * we directly input the element in it. When the cache is full we use the queue to get the last
+	 * visited element and replace it with the new element and also we place the new element in the
+	 * queue.
+	 * 
 	 * @param key
+	 *            the key for the object that is to placed in the map.
 	 * @param value
-	 * @return
+	 *            the value of the object.
+	 * @return true if the object was added to the cache.
 	 */
 	public boolean addElement(K key, V value) {
 		if (cache.containsKey(key)) {
@@ -43,22 +58,34 @@ public class LRUTailCache<K, V> {
 			Utils.printConsoleMessage("Miss");
 			return true;
 		} else {
-			K lastElement = tail.get(tail.size() - 1);
-			removeElementByKey(lastElement);
+			removeElementByKey(queue.peek());
 			insertEllment(key, value);
 			Utils.printConsoleMessage("Miss");
 			return true;
 		}
 	}
 
+	/**
+	 * Inserts element in the cache and the queue.
+	 * 
+	 * @param key
+	 *            the key to the object;
+	 * @param value
+	 *            the value of the object;
+	 */
 	private void insertEllment(K key, V value) {
 		cache.put(key, value);
-		tail.add(0, key);
+		queue.add(key);
 	}
 
 	/**
+	 * Retrieves an element of the cache by specific key if there is no object to be found it
+	 * returns null.
+	 * 
 	 * @param key
-	 * @return
+	 *            the key of the object.
+	 * @return the object which is associated with the key, if there is no object with the specific
+	 *         key @return null.
 	 */
 	public V getElement(K key) {
 		if (cache.containsKey(key)) {
@@ -68,28 +95,37 @@ public class LRUTailCache<K, V> {
 	}
 
 	/**
+	 * Removes an element from the cache by its key, if there is no key that matches returns false.
+	 * 
 	 * @param key
-	 * @return
+	 *            the key to the object we want to remove.
+	 * @return true if the object was removed otherwise returns false.
 	 */
 	public boolean removeElementByKey(K key) {
 		if (cache.containsKey(key)) {
 			cache.remove(key);
-			tail.remove(key);
+			queue.remove(key);
 			return true;
 		}
 		return false;
 	}
 
 	/**
+	 * Hits the queue that an object was used.
+	 * 
 	 * @param key
+	 *            the key of the object.
 	 */
 	private void hitElement(K key) {
-		int i = tail.indexOf(key);
-		K tmp = tail.get(i);
-		tail.remove(i);
-		tail.add(0, tmp);
+		queue.remove(key);
+		queue.add(key);
 	}
 
+	/**
+	 * Returns a collection of all the elements in the cache.
+	 * 
+	 * @return a collection of all the elements in the cache.
+	 */
 	public Collection<V> getAllElements() {
 		return cache.values();
 	}
--- JavaCourse/Collections/src/main/java/com/sirma/itt/javacourse/collections/task4/LRUcache/RunLRUCache.java
+++ JavaCourse/Collections/src/main/java/com/sirma/itt/javacourse/collections/task4/LRUcache/RunLRUCache.java
@@ -11,21 +11,26 @@ public class RunLRUCache {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		LRUCache cache = new LRUCache(150);
+		// LRUCache cache = new LRUCache(150);
 		LRUMapCache<Integer, Integer> mapCache = new LRUMapCache<Integer, Integer>(100);
+		LRUTailCache<Integer, Integer> tailCache = new LRUTailCache<Integer, Integer>(100);
 
 		for (int i = 0; i < 100220; i++) {
 			Integer tmp = Utils.generateRandomNumberWithRange(1, 500);
 			Utils.printConsoleMessage("Random generated number " + tmp);
 			// cache.addCacheEllement(tmp);
-			mapCache.addCacheEllement(tmp, tmp);
+			// mapCache.addCacheEllement(tmp, tmp);
+			tailCache.addElement(tmp, tmp);
 		}
 
 		// Utils.printConsoleMessage(cache.getAllEllements().toString());
 		// Utils.printConsoleMessage(cache.getAllEllements().size() + "");
 
-		Utils.printConsoleMessage(mapCache.getAllEllements().toString());
-		Utils.printConsoleMessage(mapCache.getAllEllements().size() + "");
+		// Utils.printConsoleMessage(mapCache.getAllElements().toString());
+		// Utils.printConsoleMessage(mapCache.getAllElements().size() + "");
+		Utils.printConsoleMessage(tailCache.getAllElements().toString());
+		Utils.printConsoleMessage(tailCache.getAllElements().size() + "");
+
 	}
 
 }
